2e8d9417e006d64d34ec03dd0ae1760b12f88e2d
Peter Krause <krausepeterm@gmail.com>
2022-02-25 10:48:33 -0300

    chore: Echo result pre-comparison

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 result=$(python3 /search_pull_request_comments.py "$EVENT_DETAILS" "$GITHUB_TOKEN" "$REQUIRED_COMMENT_USER")
 
+echo "Result:"
+echo $result
+
 if [ "$result" = "0" ]; then
     echo "User that was required to comment did leave a comment - all good!"
 else
