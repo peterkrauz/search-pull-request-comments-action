9f3afacae41f5201b49604ae84ecb44d93a66b00
Peter Krause <krausepeterm@gmail.com>
2024-01-26 14:55:54 -0300

    wip: Storing work in progress

--- core/search_pull_request_comments.py
+++ core/search_pull_request_comments.py
@@ -1,26 +1,9 @@
 import logging
-from json import loads, dumps
+from json import loads
 
 import requests
 
 logger = logging.getLogger(__name__)
-
-
-def fetch(url: str, token: str) -> list:
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "X-Github-Api-Version": "2022-11-28",
-        "Authorization": f"Bearer {token}",
-    }
-    print("Brrr:", f"Bearer {token}")
-    print("Making a request to", url)
-    print()
-    print("with headers:", headers)
-    print()
-    response = requests.get(url, headers=headers)
-    print("status:", response.status_code)
-    print()
-    return response.json()
 
 
 def search_comments(github_info, token: str, required_user):
@@ -30,28 +13,30 @@ def search_comments(github_info, token: str, required_user):
     if "event" in github_info:
         github_info = github_info["event"]
 
-    print("github_info:")
-    print(dumps(github_info, indent=4))
-    print()
-
     links = github_info["pull_request"]["_links"]
-    simple_comments = fetch(links["comments"]["href"], token)
     review_comments = fetch(links["review_comments"]["href"], token)
 
-    try:
-        all_comments = (simple_comments + review_comments)
-    except Exception:
-        print("simple_comments:", simple_comments)
-        print("review_comments:", review_comments)
-        all_comments = []
-
-    users_that_left_a_comment = [c["user"]["login"] for c in all_comments]
-    required_user_has_commented = str(required_user) in users_that_left_a_comment
+    users_that_commented = [comm["user"]["login"] for comm in review_comments]
+    required_user_has_commented = str(required_user) in users_that_commented
 
     return {
-        "comments": users_that_left_a_comment,
+        "comments": users_that_commented,
         "required_user_has_commented": required_user_has_commented
     }
+
+
+def fetch(url: str, token: str) -> list:
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-Github-Api-Version": "2022-11-28",
+        "Authorization": f"Bearer {token}",
+    }
+    print("Making a request to", url)
+    print()
+    response = requests.get(url, headers=headers)
+    print("status:", response.status_code)
+    print()
+    return response.json()
 
 
 if __name__ == "__main__":
